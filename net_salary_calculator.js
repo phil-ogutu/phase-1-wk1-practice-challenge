8b45ddbb4af31e1d2c57d1651967a95bd2e1e6f7
phil-ogutu <phil.shifwoko@gmail.com>
2025-03-09 23:15:59 +0300

    Modifies Net Salary Calculator

--- net_salary_calculator.js
+++ net_salary_calculator.js
@@ -5,7 +5,6 @@ function NetSalaryCalculator(){
     let PAYE = 0
     let nhifDeductions = 0
     let nssfDeductions = 0
-    let grossSalary = 0
 
     nhifDeductions = 0.0275 * basicSalary
 
@@ -26,15 +25,15 @@ function NetSalaryCalculator(){
         PAYE = (0.1 * 24000) + ((taxableIncome - 24000)*0.25)
     }
     else if(taxableIncome>=32334 && taxableIncome<=500000){
-        PAYE = (0.1 * 24000) + ((32333 - 24000)*0.25) + ((grossSalary - 32334)*0.30)
+        PAYE = (0.1 * 24000) + ((32333 - 24000)*0.25) + ((taxableIncome - 32334)*0.30)
     }
     else if(taxableIncome>=500001 && taxableIncome<=800000){
         PAYE = (0.1 * 24000) + ((32333 - 24000)*0.25) + ((500000 - 32333)*0.30) + ((taxableIncome - 500000)*0.325)
     }else if(taxableIncome>=800000){
-        PAYE = (0.1 * 24000) + ((32333 - 24000)*0.25) + ((500000 - 32333)*0.30) + ((800000 - 500000)*0.325) + ((grossSalary - 800000)*0.35)
+        PAYE = (0.1 * 24000) + ((32333 - 24000)*0.25) + ((500000 - 32333)*0.30) + ((800000 - 500000)*0.325) + ((taxableIncome - 800000)*0.35)
     }
 
-    grossSalary = basicSalary + benefits
+    let grossSalary = basicSalary + benefits
     const netSalary = grossSalary - (PAYE + nhifDeductions + nssfDeductions)
 
     console.log(`payee: ${PAYE}`)
